5cc769deaca55a0e48a7fdf195859cdc69f9d286
Oliver Gerlich <oliver.gerlich@gmx.de>
2026-03-05 21:47:39 +0100

    build with Ninja (for faster rebuilds during development)

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -e
 
 cmake ./../source \
       -DCMAKE_INSTALL_PREFIX=/usr/local \
+      -DCMAKE_GENERATOR=Ninja \
       -DCORE_PLATFORM_NAME=x11 \
       -DENABLE_INTERNAL_DAV1D=ON \
       -DAPP_RENDER_SYSTEM=gl \
       -DENABLE_INTERNAL_FFMPEG=ON
 
-cmake --build . -- VERBOSE=1 -j2
+cmake --build . -- -j2
