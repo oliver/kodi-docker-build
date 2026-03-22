c94f25d7f991dbe63fd2537bee84bb278eade562
Oliver Gerlich <oliver.gerlich@gmx.de>
2026-03-22 10:32:28 +0100

    add "official" to all addon-related build directory names

--- build.sh
+++ build.sh
@@ -24,23 +24,23 @@ cd /kodi/build
 
 # Download source repository for official addons
 # (this uses the repository described in /kodi/source/cmake/addons/bootstrap/repositories/binary-addons.txt)
-mkdir addons_bootstrap_cmake
-cd addons_bootstrap_cmake
+mkdir addons_official_bootstrap_cmake
+cd addons_official_bootstrap_cmake
 cmake /kodi/source/cmake/addons/bootstrap/ \
       -DCMAKE_GENERATOR=Ninja \
-      -DCMAKE_INSTALL_PREFIX=/kodi/build/addons_bootstrap_install \
-      -DBUILD_DIR=/kodi/build/addons_bootstrap_build
+      -DCMAKE_INSTALL_PREFIX=/kodi/build/addons_official_bootstrap_install \
+      -DBUILD_DIR=/kodi/build/addons_official_bootstrap_build
 cmake --build .
 
 # Build all official addons
 cd /kodi/build
-mkdir addons_build_cmake
-cd addons_build_cmake
+mkdir addons_official_build_cmake
+cd addons_official_build_cmake
 cmake /kodi/source/cmake/addons/ \
       -DCMAKE_GENERATOR=Ninja \
       -DADDONS_TO_BUILD="" \
-      -DADDONS_DEFINITION_DIR=/kodi/build/addons_bootstrap_install \
+      -DADDONS_DEFINITION_DIR=/kodi/build/addons_official_bootstrap_install \
       -DCMAKE_INSTALL_PREFIX=/kodi/build/kodi_install/share/kodi/addons/ \
-      -DBUILD_DIR=/kodi/build/addons_build_build \
+      -DBUILD_DIR=/kodi/build/addons_official_build_build \
       -DPACKAGE_ZIP=1
 cmake --build .
